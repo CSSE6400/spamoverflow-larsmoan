45b7a2dad02b5a2cde0bd877fa3815126e62bf9a
Lars Moan <lars.ostberg.moan@gmail.com>
2024-03-13 12:43:43 +1000

    Refactored into app/ and added some files from the example handed out in the docker file and local.sh

--- app/spamoverflow/__init__.py
+++ app/spamoverflow/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app(config_overrides=None): 
    app = Flask(__name__) 
 
-   from .views.routes import api
+   from spamoverflow.views.routes import api
    app.register_blueprint(api)
    
 
---
+++ local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Change to the app folder
+cd app
+
+#Buildkit to make sure we know what env
+export DOCKER_BUILDKIT=1
+
+#Build the docker container
+docker buidl -t s4827064 .
+
+#Run the docker container in the background and remove after it is closed
+docker run -d --rm -p 8080:8080 s4827064
